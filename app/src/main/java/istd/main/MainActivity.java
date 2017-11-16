cd4aa8c3eb64e96414ca6925d6f0370caf5dc047
yaojie <limyaojie93@gmail.com>
2017-11-16 10:58:05 +0800

    move to settings activity

--- app/src/main/java/istd/main/MainActivity.java
+++ app/src/main/java/istd/main/MainActivity.java
@@ -1,5 +1,6 @@
 package istd.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -44,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            this.startActivity(new Intent(this, SettingsActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
