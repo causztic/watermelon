a252e1110f7af5d70565fced036701b38127eaa1
joel-huang <huang.joel@hotmail.com>
2017-11-20 22:31:52 +0800

    home layout done, latlngs not working

--- app/src/main/java/istd/main/MainActivity.java
+++ app/src/main/java/istd/main/MainActivity.java
@@ -1,22 +1,113 @@
 package istd.main;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    LatLng currentLatLng;
+    private EditText currentLoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Change status bar color for SDK21 and above.
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.setStatusBarColor(getResources().getColor(R.color.colorPrimary));
+        }
+
+        // Do the location check.
+        if (!isLocationEnabled()) {
+            showAlert();
+        } else {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            LocationListener locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    if(location!=null) {
+                        currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+                }
+            };
+
+            if (locationManager != null) {
+                int MY_PERMISSIONS = 0;
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS);
+                    return;
+                }
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+
+            currentLoc = findViewById(R.id.CurrentLocationText);
+            if (currentLatLng==null) currentLoc.setText("FUCK LOL u got null");
+            else currentLoc.setText("is ok");
+        }
+
+        // Update the budget text to reflect the budget slider value.
+        SeekBar budgetBar = findViewById(R.id.BudgetBar);
+        ((TextView) findViewById(R.id.budgetValue)).setText("$" + Integer.toString(budgetBar.getProgress()));
+        budgetBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int updatedBudget = seekBar.getProgress();
+                if (updatedBudget < 51) {
+                    ((TextView) findViewById(R.id.budgetValue)).setText("$" + Integer.toString(updatedBudget));
+                } else {
+                    ((TextView) findViewById(R.id.budgetValue)).setText("∞");
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Button button = (Button) findViewById(R.id.mapButton);
@@ -27,27 +118,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        TextView settingsChanger = findViewById(R.id.SettingsLink);
+        settingsChanger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            }
+        });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    // Check if location enabled?
+    private boolean isLocationEnabled() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return lm.isProviderEnabled(LocationManager.GPS_PROVIDER) || lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    // Alert the user to turn on location settings if not on.
+    private void showAlert() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            this.startActivity(new Intent(MainActivity.this, SettingsActivity.class));
-        }
-
-        return super.onOptionsItemSelected(item);
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this, R.style.AlertDialogStyle);
+        dialog.setTitle("Enable Location")
+                .setMessage("\nYour Location is set to 'Off'.\n\nPlease enable Location to " +
+                        "use this app")
+                .setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        moveTaskToBack(true);
+                        android.os.Process.killProcess(android.os.Process.myPid());
+                        System.exit(1);
+                    }
+                });
+        dialog.setCancelable(false);
+        dialog.show();
     }
 }
