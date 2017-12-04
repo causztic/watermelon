679aa419edcaf3fa9ac5fa75be24361844104653
yaojie <limyaojie93@gmail.com>
2017-12-04 19:23:39 +0800

    use staging server

--- app/src/main/java/istd/graph/Graph.java
+++ app/src/main/java/istd/graph/Graph.java
@@ -28,7 +28,8 @@ import istd.main.R;
 
 public class Graph extends AsyncTask<String, Void, List<Edge>> {
 
-    private final String PHANTOM_URL = "http://10.0.2.2:3000/";
+    //private final String PHANTOM_URL = "http://10.0.2.2:3000/";
+    private final String PHANTOM_URL = "http://watermelon-phantom.herokuapp.com/";
     private Vertex root;
     private List<Vertex> vertices;
     private List<Edge> edges;
